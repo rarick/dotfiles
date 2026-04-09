c3d8402bd8b8e1fd63f561c2541233ee70ed0b8d
Tyler Rarick <tyler.rarick@gmail.com>
2026-04-09 19:37:11 +0000

    moved fisher plugins to their own path

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,9 +1,17 @@
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Keep fisher plugins separate from our config
+set -gx fisher_path ~/.local/share/fisher
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+if test -d $fisher_path/conf.d
+    for f in $fisher_path/conf.d/*.fish
+        source $f
+    end
+end
 
+if status is-interactive
     fish_config theme choose "Dracula Official"
 
     set fish_key_bindings fish_user_key_bindings
